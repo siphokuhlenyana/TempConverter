d212e4b9358d17096e9cf184fae58ef1a2b7d982
Siphokuhle <siphokuhlenyana@gmail.com>
2024-05-17 16:31:39 +0200

    edit

---
+++ script/temp.js
@@ -0,0 +1,11 @@
+let celcius =document.getElementsByTagName('input')[0]
+let fahrenheit =document.getElementsByTagName('input')[1]
+let button=document.getElementsByTagName('button')
+
+button.addEventListener('click',()=>{
+
+})
+
+function convertToFahrenheit(){
+    temparature.value=(parseFloat(celcius.value)*9) /5
+}
