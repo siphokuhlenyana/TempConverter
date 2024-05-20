768eb9685c034097f20e72d8f9c8fbe74ebaf91c
Siphokuhle <siphokuhlenyana@gmail.com>
2024-05-20 11:46:20 +0200

    Added

--- script/temp.js
+++ script/temp.js
@@ -1,11 +1,18 @@
 let celcius =document.getElementsByTagName('input')[0]
 let fahrenheit =document.getElementsByTagName('input')[1]
-let button=document.getElementsByTagName('button')
+let kelvin =document.getElementsByTagName('input')[2]
+let button=document.getElementsByTagName('button')[0]
 
 button.addEventListener('click',()=>{
+    convertToFahrenheit()
+    convertToKelvin()
 
 })
 
 function convertToFahrenheit(){
-    temparature.value=(parseFloat(celcius.value)*9) /5
+    fahrenheit.value=(parseFloat(celcius.value)*9) /5
+}
+
+function convertToKelvin(){
+kelvin.value=(parseFloat(celcius.value)+ 273.15)
 }
